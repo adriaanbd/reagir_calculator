6842dcfd46a7075f5b079e31a8082b51ac80f65e
Adriaan Beiertz <adriaanbd@gmail.com>
2019-12-12 22:46:06 -0500

    adding operate module with basic tests

---
+++ src/logic/operate.js
@@ -0,0 +1,16 @@
+import Big from 'big.js';
+
+function operate(num1, num2, operator) {
+  const [a, b] = [Big(num1), Big(num2)];
+  let result;
+
+  if (operator === '-') result = a.minus(b);
+  if (operator === '+') result = a.plus(b);
+  if (operator === '÷') result = a.div(b);
+  if (operator === 'X') result = a.times(b);
+  if (operator === '%') result = a.mod(b);
+
+  return result.toString();
+}
+
+export default operate;
